5041995f6476eba6f8cd3cd9c68accede3b218fe
Angel Velasco <avelasco920@gmail.com>
2017-11-21 10:59:30 -0800

    creaetd user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,11 @@ class User < ApplicationRecord
   validates :username, :email, presence: true, uniqueness: true
   validates :session_token, :password_digest, :contact_number,
             :stocks_value, :cash_value, :portfolio_value,
-            :img_url, presence: true
+            presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
-  after_initialize :ensure_session_token
+  after_initialize :ensure_session_token, :calculate_portfolio
 
   def password=(password)
     @password = password
@@ -32,9 +32,38 @@ class User < ApplicationRecord
   end
 
   def valid_password?(password)
-
+    BCrypt::Password.new(password_diget).is_password?(password)
   end
 
-  def 
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  def calculate_cash_value
+    self.cash_value = self.portfolio_value - 15
+  end
+
+  def process_stock_value
+    # self.stocks_value ||= return 15
+  end
+
+  def calculate_portfolio
+     # replace stock value
+     # process_stock_value
+    self.stocks_value = 15
+    calculate_cash_value
+  end
 
 end
