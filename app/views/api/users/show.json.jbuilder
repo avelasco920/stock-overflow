7ea013451adb8a63ac7af45bedc1f91d24d99d35
Angel Velasco <avelasco920@gmail.com>
2017-11-21 20:16:19 -0800

    added protected routes

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,16 +1,1 @@
-json.set! @user.id do
-  json.extract! @user, :id, :username, :first_name,
-                :last_name, :email, :contact_number,
-                :img_url, :stocks_value, :cash_value,
-                :portfolio_value
-
-  json.companies_watching do
-    json.array! @user.companies_watching.pluck(:id)
-  end
-
-  json.stocks do
-    @user.stocks.each do |stock|
-      json.set! stock.company_id, stock.num_shares
-    end
-  end
-end
+json.partial! "api/users/user", user: @user
---
+++ frontend/components/app.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
+import SearchContainer from './search/search_container';
+import BenchShowContainer from './bench_show/bench_show_container';
+import BenchFormContainer from './bench_form/bench_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/" className="header-link">
+        <h1>Bench BnB</h1>
+      </Link>
+      <GreetingContainer />
+    </header>
+    <Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+      <ProtectedRoute path="/benches/new" component={BenchFormContainer} />
+      <Route path="/benches/:benchId" component={BenchShowContainer} />
+      <Route exact path="/" component={SearchContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
