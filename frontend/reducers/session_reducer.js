9e42203fd93ad2e1acb740f604774184a436fc80
Angel Velasco <avelasco920@gmail.com>
2017-11-21 17:20:54 -0800

    completed sessions reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,22 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_CURRENT_USER,
+} from '../actions/session_actions';
+
+const defaultState = {
+  currentUser: null,
+};
+
+const sessionReducer = (state = defaultState, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, { currentUser });
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
--- frontend/stock_overflow.jsx
+++ frontend/stock_overflow.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import {
   signup,
   login,
@@ -7,9 +8,24 @@ import {
 } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  // testing...
   window.signup = signup;
   window.login = login;
   window.logout = logout;
+  //
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to StockOverflow</h1>, root);
 });
