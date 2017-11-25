307007dcfd994f12fce32eae27d974d3f9ac4e15
Angel Velasco <avelasco920@gmail.com>
2017-11-25 14:42:28 -0800

    created news articles reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,12 +2,12 @@ import { combineReducers } from 'redux';
 
 import companies from './companies_reducer';
 import user from './user_reducer';
-// import newsArticles from './news_articles_reducer';
+import newsArticles from './news_articles_reducer';
 
 const entitiesReducer = combineReducers({
   companies,
   user,
-  // newsArticles,
+  newsArticles,
 });
 
 export default entitiesReducer;
---
+++ frontend/reducers/news_articles_reducer.js
@@ -0,0 +1,22 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_NEWS_ARTICLES,
+  RECEIVE_NEWS_ARTICLE,
+} from '../actions/news_articles_actions';
+
+const newsArticlesReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_NEWS_ARTICLES:
+      return merge({}, action.newsArticles);
+    case RECEIVE_NEWS_ARTICLE:
+      const newsArticles = merge({}, state);
+      const newsArticle = action.newsArticle;
+      return merge({}, newsArticles, newsArticle);
+    default:
+      return state;
+  }
+};
+
+export default newsArticlesReducer;
