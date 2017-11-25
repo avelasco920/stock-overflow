320658bb188abba3b8003556bb4e3935fcdf2e37
Angel Velasco <avelasco920@gmail.com>
2017-11-24 16:31:43 -0800

    edited media specific styling

--- frontend/components/home_page/stock_index_item.js
+++ frontend/components/home_page/stock_index_item.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../../util/route_util';
 import { Link, withRouter } from 'react-router-dom';
 
-const StockIndexItem = ({ company }) => (
-  <li className="stock-index-item">
-    <div className="stock-text">
-      <h4 className="stock-symbol">{company.symbol}</h4>
-    <span className="stock-num-shares">{company.num_shares} Shares</span>
-    </div>
-    <img
-      src="https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/Screen%20Shot%202017-11-24%20at%203.30.55%20PM.png?raw=true"
-      className="chart-thumb"/>
-    <span className="stock-price">${company.market_price}</span>
-  </li>
-);
+const StockIndexItem = ({ company }) => {
+  const marketPrice = parseFloat(Math.round(company.market_price * 100) / 100).toFixed(2);
+  return (
+    <li className="stock-index-item">
+      <div className="stock-text">
+        <h4 className="stock-symbol">{company.symbol}</h4>
+        <span className="stock-num-shares">{company.num_shares} Shares</span>
+      </div>
+      <img
+        src="https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/Screen%20Shot%202017-11-24%20at%203.30.55%20PM.png?raw=true"
+        className="chart-thumb"/>
+      <span className="stock-price">${marketPrice}</span>
+    </li>
+  );
+};
 
 export default StockIndexItem;
