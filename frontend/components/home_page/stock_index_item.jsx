c6301242d29961d1782ca50a0f41042e402108f0
Angel Velasco <avelasco920@gmail.com>
2017-11-25 19:55:35 -0800

    changed graph images

--- frontend/components/home_page/stock_index_item.jsx
+++ frontend/components/home_page/stock_index_item.jsx
@@ -4,6 +4,17 @@ import { Link, withRouter } from 'react-router-dom';
 import { stringifyIntegerNoCommas } from '../../util/parsing_functions';
 
 const StockIndexItem = ({ company }) => {
+  const graphUrl = [
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green1.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green2.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green3.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green4.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red1.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red2.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red3.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red4.png?raw=true"
+  ];
+  const randGraph = graphUrl[Math.floor(Math.random() * graphUrl.length)];
   return (
     <li className="stock-index-item">
       <div className="stock-text">
@@ -11,7 +22,7 @@ const StockIndexItem = ({ company }) => {
         <span className="stock-num-shares">{company.num_shares} Shares</span>
       </div>
       <img
-        src="https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/Screen%20Shot%202017-11-24%20at%203.30.55%20PM.png?raw=true"
+        src={randGraph}
         className="chart-thumb"/>
       <span className="stock-price">
         ${stringifyIntegerNoCommas(company.market_price)}
--- frontend/components/home_page/watchlist_index_item.jsx
+++ frontend/components/home_page/watchlist_index_item.jsx
@@ -4,13 +4,24 @@ import { Link, withRouter } from 'react-router-dom';
 import { stringifyIntegerNoCommas } from '../../util/parsing_functions';
 
 const WatchlistIndexItem = ({ company }) => {
+  const graphUrl = [
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green1.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green2.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green3.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green4.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red1.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red2.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red3.png?raw=true",
+    "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/red4.png?raw=true"
+  ];
+  const randGraph = graphUrl[Math.floor(Math.random() * graphUrl.length)];
   return (
     <li className="stock-index-item">
       <div className="stock-text">
         <h4 className="stock-symbol">{company.symbol}</h4>
       </div>
       <img
-        src="https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/Screen%20Shot%202017-11-24%20at%203.30.55%20PM.png?raw=true"
+        src={randGraph}
         className="chart-thumb"/>
       <span className="stock-price">
         ${stringifyIntegerNoCommas(company.market_price)}
