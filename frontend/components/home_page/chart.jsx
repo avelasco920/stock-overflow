48df5b5f1f23d9105ca003a7eba21ef344de5f3c
Angel Velasco <avelasco920@gmail.com>
2017-11-30 21:08:16 -0800

    reduced chart dimensions

--- frontend/components/home_page/chart.jsx
+++ frontend/components/home_page/chart.jsx
@@ -5,13 +5,33 @@ import { Link, withRouter } from 'react-router-dom';
 import ChartOverlay from './chart_overlay';
 
 class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgUrl: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green3.png?raw=true",
+    };
+  }
+
+  changeImage(num) {
+    const imgs = {
+      1: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green2.png?raw=true",
+      2: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green3.png?raw=true",
+      4: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green4.png?raw=true",
+      5: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green5.png?raw=true",
+      6: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green6.png?raw=true",
+      7: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green7.png?raw=true",
+    };
+    this.setState({imgUrl: imgs[num]});
+  }
+
   render() {
     const { user } = this.props;
+    const { imgUrl } = this.state;
     return (
       <div className="chart">
         <ChartOverlay user={user}/>
         <img
-          src="https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green3.png?raw=true"
+          src={imgUrl}
           className="chart-placeholder"/>
       </div>
     );
