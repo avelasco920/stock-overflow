24bddd1eec1f1b9629482396a3dc029d0d865c79
Angel Velasco <avelasco920@gmail.com>
2017-11-30 21:24:29 -0800

    image adjustments

--- frontend/components/home_page/chart.jsx
+++ frontend/components/home_page/chart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../../util/route_util';
 import { Link, withRouter } from 'react-router-dom';
+import { stringifyToFloat } from '../../util/parsing_functions';
 
 import ChartOverlay from './chart_overlay';
 
@@ -8,20 +9,13 @@ class Chart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgUrl: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green3.png?raw=true",
+      imgUrl: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green2.png?raw=true",
     };
   }
 
   changeImage(num) {
-    const imgs = {
-      1: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green2.png?raw=true",
-      2: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green3.png?raw=true",
-      4: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green4.png?raw=true",
-      5: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green5.png?raw=true",
-      6: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green6.png?raw=true",
-      7: "https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green7.png?raw=true",
-    };
-    this.setState({imgUrl: imgs[num]});
+    const url = `https://github.com/avelasco920/stocks-overflow/blob/master/app/assets/images/chart%20thumbnail/green${num}.png?raw=true`;
+    this.setState({imgUrl: url});
   }
 
   render() {
@@ -29,7 +23,22 @@ class Chart extends React.Component {
     const { imgUrl } = this.state;
     return (
       <div className="chart">
-        <ChartOverlay user={user}/>
+        <div className="chart-overlay">
+          <div className="account-info">
+            <h2>${stringifyToFloat(user.portfolio_value)}</h2>
+            <div className="delta">
+              <h4>+$7.01(+0.70%)</h4> <span>Today</span>
+            </div>
+          </div>
+          <div className="chart-history">
+            <a onClick={() => this.changeImage(1)}>Today</a>
+            <a onClick={() => this.changeImage(2)}>1W</a>
+            <a onClick={() => this.changeImage(3)}>1M</a>
+            <a onClick={() => this.changeImage(4)}>3M</a>
+            <a onClick={() => this.changeImage(5)}>1Y</a>
+            <a onClick={() => this.changeImage(6)}>5Y</a>
+          </div>
+        </div>
         <img
           src={imgUrl}
           className="chart-placeholder"/>
